fb18884a4dd5e65893bcb4eeaae0ac28048d638b
Oscar J. Irún <oscarjiv91@gmail.com>
2021-01-04 19:20:00 -0300

    sitecare updates

--- web/app/plugins/gravityforms/gravityforms.php
+++ web/app/plugins/gravityforms/gravityforms.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravity Forms
 Plugin URI: https://gravityforms.com
 Description: Easily create web forms and manage form entries within the WordPress admin.
-Version: 2.4.21
+Version: 2.4.22
 Author: Gravity Forms
 Author URI: https://gravityforms.com
 License: GPL-2.0+
@@ -133,7 +133,7 @@ define( 'GF_SUPPORTED_WP_VERSION', version_compare( get_bloginfo( 'version' ), G
  *
  * @var string GF_MIN_WP_VERSION_SUPPORT_TERMS The version number
  */
-define( 'GF_MIN_WP_VERSION_SUPPORT_TERMS', '5.3' );
+define( 'GF_MIN_WP_VERSION_SUPPORT_TERMS', '5.5' );
 
 
 if ( ! defined( 'GRAVITY_MANAGER_URL' ) ) {
@@ -210,7 +210,7 @@ class GFForms {
 	 *
 	 * @var string $version The version number.
 	 */
-	public static $version = '2.4.21';
+	public static $version = '2.4.22';
 
 	/**
 	 * Handles background upgrade tasks.
