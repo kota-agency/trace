a1920d5fc79a328b83cb08308ce55e984d8b1b7e
Oscar J. Irún <oscarjiv91@gmail.com>
2021-01-18 14:08:39 -0300

    sitecare updates

--- web/app/plugins/redirection/redirection-version.php
+++ web/app/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.9.2' );
-define( 'REDIRECTION_BUILD', 'b1c9e2f6a1c3f8b6cc251a44757c6863' );
+define( 'REDIRECTION_VERSION', '5.0' );
+define( 'REDIRECTION_BUILD', '3451e9c29beac3055f61a8e45f1bf15e' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
