4d3fa00ea8ad1e59d8b7e2e72abecb20a46154fd
Oscar Irun <oscarjiv91@gmail.com>
2022-05-09 19:38:08 -0400

    fix

--- web/app/themes/trace/temp-services.php
+++ web/app/themes/trace/temp-services.php
@@ -93,7 +93,7 @@ if($intro_copy2) {
 ?>
 
 <section class="text-layout section-two-image-position <?= implode(' ', $classes2); ?>">
-	<div class="container" data-aos="fade">
+	<div class="container" data-aos="fade" >
 		<?php if ($background_text2) : ?>
 			<h2 class="background-text background-text--large"><?= $background_text2; ?></h2>
 		<?php endif; ?>
@@ -145,7 +145,7 @@ if($intro_copy2) {
 				<?php endif; ?>
 			<?php else : ?>
 				<?php if ($split_level2) : ?>
-					<div class="row text-layout__split-row" data-aos="fade" data-aos-offset="-500">
+					<div class="row text-layout__split-row" data-aos="fade">
 						<div class="col-md-9">
 							<?php if ($heading2) : ?>
 								<h2><?= $heading2; ?></h2>
@@ -570,7 +570,7 @@ if($intro_copy5) {
 							</div>
 						<?php endif; ?>
 					</div>
-					<div class="col-md-6 col-lg-5" data-aos="fade">
+					<div class="col-md-6 col-lg-4" data-aos="fade">
 						<?php if (have_rows('text_layout_c_columns')) : ?>
 							<div class="row">
 								<?php while (have_rows('text_layout_c_columns')) : the_row(); ?>
