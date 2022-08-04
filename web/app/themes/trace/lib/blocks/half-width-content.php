a36d6691d13bf943171f3e9dbf7b5c1c0b7f7b9a
Oscar Irun <oscarjiv91@gmail.com>
2022-08-03 21:22:37 -0400

    fixed image in half width content

--- web/app/themes/trace/lib/blocks/half-width-content.php
+++ web/app/themes/trace/lib/blocks/half-width-content.php
@@ -18,8 +18,8 @@ $layout = get_sub_field('layout');
 <?php if ($copy) : ?>
     <section <?= block_id(); ?> class="half-width-content half-width-content--<?= $layout ?> <?= implode(' ', $classes); ?>" data-aos="fade">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-6 <?php if($layout == 'quote-left') : ?> offset-lg-6 <?php endif; ?> <?php if ($offset) : ?>offset-lg-1<?php endif; ?> order-2 order-lg-1 last-margin">
+            <div class="row <?php if($layout == 'quote-left') : ?> flex-lg-row-reverse <?php endif; ?>">
+                <div class="col-lg-6 <?php if ($offset) : ?>offset-lg-1<?php endif; ?> order-2 order-lg-1 last-margin">
                     <?= $copy; ?>
                 </div>
                 <div class="col-lg-5 order-1 order-lg-2 
