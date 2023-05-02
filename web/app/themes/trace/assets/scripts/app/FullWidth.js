616236835de3ef1ac60161ffd73a8c6af8cbd0da
Oscar Irun <oscarjiv91@gmail.com>
2023-05-02 15:01:49 -0400

    fix for safari

--- web/app/themes/trace/assets/scripts/app/FullWidth.js
+++ web/app/themes/trace/assets/scripts/app/FullWidth.js
@@ -1,4 +1,6 @@
 import scrollbarWidth from "../utilities/scrollbarWidth";
+var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+var is_safari = navigator.userAgent.indexOf("Safari") > -1;
 
 function FullWidth() {
 
@@ -11,11 +13,25 @@ function FullWidth() {
     const siteMain = document.querySelector('.site-wrapper');
 
 
+    let safariAgent = userAgentString.indexOf("Safari") > -1;
+
     fullWidth.forEach( (el) => {
-        el.style.width = (window.innerWidth - scrollbarWidth()) + 'px';
-        el.style.marginLeft = -siteMain.getBoundingClientRect().left + 'px';
+        if(!isSafariBrowser) {
+            el.style.width = (window.innerWidth - scrollbarWidth()) + 'px';
+            el.style.marginLeft = -siteMain.getBoundingClientRect().left + 'px';
+        }
         $(el).addClass('width-calculated');
     });
+}
+
+function isSafariBrowser(){
+    if (is_safari){
+        if (is_chrome)  // Chrome seems to have both Chrome and Safari userAgents
+            return false;
+        else
+            return true;
+    }
+    return false;
 }
 
 export default FullWidth;
