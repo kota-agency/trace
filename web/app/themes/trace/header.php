c782895f1bd059b791aa3f6d748badc4cafca667
alvasilakis <al.vasilakis@outlook.com>
2024-04-02 15:40:37 +0300

    Add Hotjar Script

--- web/app/themes/trace/header.php
+++ web/app/themes/trace/header.php
@@ -23,6 +23,18 @@
     gtag('config', 'UA-5740928-1');
     </script>
 
+    <!-- Hotjar Tracking Code for Trace Solutions -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3928783,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
 
     <!-- Global site tag (gtag.js) - Google Ads --> 
     <script async src=https://www.googletagmanager.com/gtag/js?id=AW-773370370></script>
