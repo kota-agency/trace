1f521a02ba541c49347ecdd54431f601121bd6b0
Oscar Irun <oscarjiv91@gmail.com>
2024-08-12 09:03:23 -0400

    removed unused gtm

--- web/app/themes/trace/header.php
+++ web/app/themes/trace/header.php
@@ -11,14 +11,6 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TLL3QZN');</script>
-    <!-- End Google Tag Manager -->
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-P2RBSHNF');</script>
     <!-- End Google Tag Manager -->
 
@@ -91,10 +83,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TLL3QZN" height="0" width="0"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2RBSHNF" height="0" width="0"></iframe></noscript>
